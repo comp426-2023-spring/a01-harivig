ba48e3cf0cfe5eee4c271e4bfb31d378452c5fe6
harivig <54375773+harivig@users.noreply.github.com>
2023-02-27 17:29:03 -0500

    err

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const port = args.port || 3000;
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-fs.readFile('./public/index.html','utf8',(error,data) => {
-    if(error) {
-        console.error(error);
+fs.readFile('./public/index.html','utf8',(err,data) => {
+    if(err) {
+        console.error(err);
     return;    
 }
 
