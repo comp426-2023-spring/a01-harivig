959ce90fc1578d370c63613fe544b8df9f9251a8
harivig <54375773+harivig@users.noreply.github.com>
2023-02-27 17:19:52 -0500

    server port

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const port = args.port || 3000;
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-
+fs.readFile('.public/index.html','utf8',(error,data) => {
+    if(err) {
+        console.error(err);
+    return;    
+}
 
 
 
@@ -28,14 +32,21 @@ const port = args.port || 3000;
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
+const server = http.createServer((req,res) => {
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
+    res.end(data);
+})
 
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
+server.listen(port,() =>{
+    console.log(`Server listening on port ${port}`);
+})
+});
 
 
 // That's it! You're all done!
